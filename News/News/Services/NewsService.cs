5c9f5dbf1a99d9636fe05a0dc7242735a7a6e67d
Martin Lenart <martin_lenart@icloud.com>
2022-04-04 19:41:56 +0200

    Add project files.

---
+++ News/News/Services/NewsService.cs
@@ -0,0 +1,39 @@
+﻿#define UseNewsApiSample  // Remove or undefine to use your own code to read live data
+
+using System;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Collections.Concurrent;
+using System.Threading.Tasks;
+
+using News.Models;
+using News.ModelsSampleData;
+
+namespace News.Services
+{
+    public class NewsService
+    {
+
+        //Here is where you lift in your Service code from Part A
+/*
+        public async Task<NewsGroup> GetNewsAsync(NewsCategory category)
+        {
+
+#if UseNewsApiSample      
+            NewsApiData nd = await NewsApiSampleData.GetNewsApiSampleAsync(category);
+
+#else
+            //https://newsapi.org/docs/endpoints/top-headlines
+            var uri = $"https://newsapi.org/v2/top-headlines?country=se&category={category}&apiKey={apiKey}";
+
+
+            //Recommend to use Newtonsoft Json Deserializer as it works best with Android
+            var webclient = new WebClient();
+            var json = await webclient.DownloadStringTaskAsync(uri);
+            NewsApiData nd = Newtonsoft.Json.JsonConvert.DeserializeObject<NewsApiData>(json);
+
+#endif
+*/
+    }
+}
---
+++ News/News/Views/ArticleView.xaml.cs
@@ -0,0 +1,22 @@
+﻿using System.Web;
+using Xamarin.Forms;
+
+namespace News.Views
+{
+    public partial class ArticleView : ContentPage
+    {
+        //Here is where you show the news in Full page
+        public ArticleView()
+        {
+            InitializeComponent();
+         }
+        public ArticleView(string Url)
+        {
+            InitializeComponent();
+            BindingContext = new UrlWebViewSource
+            {
+                Url = HttpUtility.UrlDecode(Url)
+            };
+        }
+    }
+}
